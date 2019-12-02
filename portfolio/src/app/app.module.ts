9cbd4352d1bd96271fffc5ae0699fb491ec9a5a0
Mike Z <mzapata2991@gmail.com>
2019-12-01 19:30:19 -0600

    added socail buttons

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CanvasComponent } from './canvas/canvas.component';
+import { TopLinksComponent } from './top-links/top-links.component';
   
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CanvasComponent
+    CanvasComponent,
+    TopLinksComponent
   ],
   imports: [
     BrowserModule,
