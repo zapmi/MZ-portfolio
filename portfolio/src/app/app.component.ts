78b7f5abdb96ac5c7915fb28b19726073e9dcc49
Mike Z <mzapata2991@gmail.com>
2019-12-02 19:19:03 -0600

    moved social links to main page instead of canvas, routes set up, main button goes to second page

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  preventDefault() {
+    event.preventDefault();
+  }
   title = 'my-app';
+
+  onClick(event: Event) {
+    event.preventDefault();
+  }
 }
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CanvasComponent } from './canvas/canvas.component';
 
+const appRoutes: Routes = [
+  { path: '', component: CanvasComponent },
+  { path: 'header', component: HeaderComponent }
+];
 
 @NgModule({
   declarations: [
@@ -17,6 +22,7 @@ import { CanvasComponent } from './canvas/canvas.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
     NgbModule
   ],
   providers: [],
